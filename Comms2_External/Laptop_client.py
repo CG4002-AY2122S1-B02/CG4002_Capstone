92685b084b3a12b1009b49211d3834e534e08f79
JOSHTAM <E0325893@u.nus.edu>
2021-09-16 09:41:38 +0800

    Updated Laptop_Client.py

--- Comms2_External/Laptop_client.py
+++ Comms2_External/Laptop_client.py
@@ -1,8 +1,7 @@
-# Test Script to send data (one-way communication) from Ultra96 FPGA to Database Server
+# Test Script to send data (two-way communication) from Laptop to Ultra96 Server
 # Implementation using Python Socket API
 
 import sys
-import argparse
 import socket
 import time
 import base64
@@ -11,12 +10,15 @@ from Crypto import Random
 from Crypto.Cipher import AES
 
 # Initialise Socket Information
-# IP_ADDR = '127.0.0.1'
-# PORT = 8001
+IP_ADDR = '127.0.0.1'
+PORT_NUM = [8001,8002,8003]
+GROUP_ID = 2
+SECRET_KEY = 'cg40024002group2'
 
 # Initialise Global Variables
 BLOCK_SIZE = 16
 PADDING = ' '
+FORMAT = 'utf8'
 
 class Client():
     def __init__(self, ip_addr, port_num, group_id, secret_key):
@@ -38,8 +40,7 @@ class Client():
         return padded_message
 
     def encrypt_message(self, dancer_id, RRT, offset, raw_data):
-        raw_message = '#' + dancer_id + '|' + RRT + '|' + offset + '|' + raw_data
-        print("Message to Encrypt: ", raw_message)
+        raw_message = '#' + str(dancer_id) + '|' + str(RRT) + '|' + str(offset) + '|' + raw_data
         padded_message = self.add_padding(raw_message)
         iv = Random.new().read(AES.block_size)
         aes_key = bytes(str(self.secret_key), encoding="utf8")
@@ -50,8 +51,12 @@ class Client():
     def send_data(self, dancer_id, RRT, offset, raw_data):
         # raw_message = raw_message.encode("utf8")
         encrypted_message = self.encrypt_message(dancer_id, RRT, offset, raw_data)
-        print("Sending data to Evaluation Server", encrypted_message)
         self.socket.sendall(encrypted_message)
+
+    def receive_timestamp(self):
+        message = self.socket.recv(1024)
+        timestamp = message.decode(FORMAT)
+        return timestamp
 
     def stop(self):
         self.connection.close()
@@ -59,26 +64,48 @@ class Client():
         self.timer.cancel()
 
 def main():
-    if len(sys.argv) != 5:
+    if len(sys.argv) != 2:
         print('Invalid number of arguments')
-        print('python Laptop_client.py [IP address] [Port] [groupID] [secret_key]')
+        print('python Laptop_client.py [dancer_id]')
         sys.exit()
 
-    ip_addr = sys.argv[1]
-    port_num = int(sys.argv[2])
-    group_id = sys.argv[3]
-    secret_key = sys.argv[4]
+    dancer_id = int(sys.argv[1])
+    ip_addr = '127.0.0.1'
+    port_num = PORT_NUM[dancer_id-1]
+    group_id = GROUP_ID
+    secret_key = SECRET_KEY
 
     my_client = Client(ip_addr, port_num, group_id, secret_key)
-    action = ""
     time.sleep(10)
 
+    RTT = 0.0
+    offset = 0.0
+
+    # Simulate Data sent to Laptop every 5 seconds for 5 times
     count = 0
+    action = ' '
+
     while action != "logout":
-        # Send the Ultra96 the received data from the 3 laptops
-        # my_client.send_data("# 1 | 4.5 | 1.5 | raw_data")
-        my_client.send_data("1", "4.5", "1.5", "raw_data")
-        time.sleep(2)
+        # Timestamp t1: When laptop receives a packet from Bluno Beetle
+        t1 = time.time()
+        print('Sending Time Sync Packet. Timestamp t1: ', str(t1))
+        my_client.send_data(dancer_id, RTT, offset, "raw_data")
+        timestamp = my_client.receive_timestamp()
+        # Timestamp t4: When laptop receives time sync response from Ultra96
+        t4 = time.time()
+        t2 = float(timestamp.split('|')[0])
+        print('Ultra96 received Packet.  Timestamp t2: ', str(t2))
+        t3 = float(timestamp.split('|')[1])
+        print('Ultra96 sent response.    Timestamp t3: ', str(t3))
+        print('Received Timestamp.       Timestamp t4: ', str(t4))
+        
+        RTT = (t2 - t1) - (t3 - t4)
+        print('Round Trip Time: ', str(RTT))
+
+        offset = (t2 - t1) - RTT/2
+        print('Clock Offset: ', str(offset))
+
+        time.sleep(5)
         count += 1
         if (count == 5):
             my_client.stop()
