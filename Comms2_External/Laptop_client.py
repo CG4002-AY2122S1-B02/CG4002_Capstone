513ce389f44385d46fbf688db14cd8363de1c973
JOSHTAM <E0325893@u.nus.edu>
2021-09-25 23:26:04 +0800

    Updated Laptop_client and Ultra96_server code

--- Comms2_External/Laptop_client.py
+++ Comms2_External/Laptop_client.py
@@ -1,6 +1,7 @@
-# Test Script to send data (two-way communication) from Laptop to Ultra96 Server
+# Script to send data (two-way communication) from Laptop to Ultra96 Server
 # Implementation using Python Socket API
 
+import ntplib
 import sys
 import socket
 import time
@@ -8,6 +9,19 @@ import base64
 
 from Crypto import Random
 from Crypto.Cipher import AES
+
+import paramiko
+import sshtunnel
+from paramiko import SSHClient
+
+# Initialise SSHTunnel Connection Information
+TUNNEL_ONE_SSH_ADDR = "sunfire.comp.nus.edu.sg"
+TUNNEL_ONE_SSH_USERNAME = "e0325893"
+TUNNEL_ONE_SSH_PASSWORD = "iLoveCapstoneB02"
+
+TUNNEL_TWO_SSH_ADDR = "137.132.86.225"
+TUNNEL_TWO_SSH_USERNAME = "xilinx"
+TUNNEL_TWO_SSH_PASSWORD = "cg4002b02"
 
 # Initialise Socket Information
 IP_ADDR = '127.0.0.1'
@@ -25,14 +39,50 @@ class Client():
         super(Client, self).__init__()
 
         # Create a TCP/IP socket and connect to database server
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_address = (ip_addr, port_num)
+        self.port_num = port_num
+        self.ip_addr = ip_addr
         self.group_id = group_id
         self.secret_key = secret_key
 
-        print('trying to connect to %s port %s' % server_address)
-        self.socket.connect(server_address)
-        print("Successfully connected to the Ultra96 server")
+    '''
+    Create a "Double-Hop" SSH Tunnel for Dancer's Laptops to Reach Ultra96 Server
+
+    | LOCAL CLIENT | <====Firewall====> | REMOTE SERVER | <==local==> | PRIVATE SERVER |
+    |    LAPTOP    | <==SSH==Port 22==> |  NUS Sunfire  | <===SSH===> |    Ultra 96    |
+    '''
+
+    def start_ssh_tunnel(self):
+        # Create Tunnel One from Laptop into NUS Sunfire Server
+        tunnel_one =  sshtunnel.open_tunnel(
+           (TUNNEL_ONE_SSH_ADDR,22), # Remote Server IP
+            ssh_username=TUNNEL_ONE_SSH_USERNAME,
+            ssh_password=TUNNEL_ONE_SSH_PASSWORD,
+            remote_bind_address=(TUNNEL_TWO_SSH_ADDR,22), # Private Server IP
+        )
+        tunnel_one.start()
+        print('Connection to tunnel_one (sunfire:22) OK...')
+
+        # Create Tunnel Two from Sunfire Server to Ultra96 Server
+        tunnel_two = sshtunnel.open_tunnel(
+            ssh_address_or_host=('127.0.0.1',tunnel_one.local_bind_port),
+            remote_bind_address=('127.0.0.1',self.port_num), # Local bind port
+            ssh_username=TUNNEL_TWO_SSH_USERNAME,
+            ssh_password=TUNNEL_TWO_SSH_PASSWORD,
+            local_bind_address=('127.0.0.1',self.port_num) # Local bind port
+        )
+        tunnel_two.start()
+        print('Connection to tunnel_two (137.132.86.225:8001) OK...')
+
+    def run(self):
+        self.start_ssh_tunnel()
+        server_address = (self.ip_addr, self.port_num)
+        print('Trying to connect to %s port %s' % server_address)    
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect(server_address)
+            print("Successfully connected to the Ultra96 server")
+        except Exception as e:
+            print('An error has occured when trying to connect to Ultra96 Server.')
 
     def add_padding(self, message):
         pad = lambda s: s + (BLOCK_SIZE - (len(s) % BLOCK_SIZE)) * PADDING
@@ -58,6 +108,17 @@ class Client():
         timestamp = message.decode(FORMAT)
         return timestamp
 
+    '''
+    Get time from NTP Server and calculate offset with system(laptop) clock
+    '''
+    def ntp_time_sync(self):
+        try:
+            ntp_client = ntplib.NTPClient()
+            response = ntp_client.request('pool.ntp.org')
+            print(responsed.tx_time)
+        except Exception as e:
+            return None
+
     def stop(self):
         self.connection.close()
         self.shutdown.set()
@@ -76,6 +137,8 @@ def main():
     secret_key = SECRET_KEY
 
     my_client = Client(ip_addr, port_num, group_id, secret_key)
+    my_client.run()
+    my_client.ntp_time_sync()
     time.sleep(10)
 
     RTT = 0.0
@@ -89,6 +152,8 @@ def main():
         # Timestamp t1: When laptop receives a packet from Bluno Beetle
         t1 = time.time()
         print('Sending Time Sync Packet. Timestamp t1: ', str(t1))
+
+
         my_client.send_data(dancer_id, RTT, offset, "raw_data")
         timestamp = my_client.receive_timestamp()
         # Timestamp t4: When laptop receives time sync response from Ultra96
--- Comms2_External/Ultra96_server.py
+++ Comms2_External/Ultra96_server.py
@@ -1,121 +1,81 @@
-# Python script on Ultra96 Server to handle communication from each Laptop
+# Test Script to receive connection on Ultra96 from Laptops 
 
 import argparse
-import base64
-import random
-import socket
 import sys
-import time
+import socket
 import threading
+import time
+import base64
 
 from Crypto import Random
 from Crypto.Cipher import AES
 
-# Initialize global values
+# Initialise Variable Sizes
+MESSAGE_SIZE = 4 # dancer_id | RTT | offset | raw_data 
 FORMAT = 'utf8'
-PADDING = ' '
-BLOCK_SIZE = 16
-MESSAGE_SIZE = 4 # dancer_id, RTT, offset, raw_data
 
-# Initialize values for Ultra96 Server 
-IP_ADDRESS = "127.0.0.1"
-PORT_NUM = [8081, 8082, 8083]
+# Assign the host and port numbers of our Ultra96 Server 
+IP_ADDR = '127.0.0.1'
+PORT_NUM = [8001,8002,8003]
 GROUP_ID = 2
 
 class Server(threading.Thread):
-    def __init__(
-        self,
-        ip_addr,
-        port_num,
-        group_id,
-        secret_key
-    ):
+    def __init__(self, ip_addr, port_num, group_id, secret_key):
         super(Server, self).__init__()
 
-        # Time stamps
-        # Indicate the time when the server receive the package
+        # Create timestamps
+        # Timestamp t2: When Ultra96 receives a sync packet from a Laptop
         self.t2 = 0
-        # Indicate the time when the server send the package
+        # Timestamp t3: When Ultra96 sends a response packet to the Laptop
         self.t3 = 0
+
+        # self.secret_key = secret_key
 
         # Create a TCP/IP socket and bind to port
         self.shutdown = threading.Event()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_address = (ip_addr, port_num)
 
-        print("starting up on %s port %s" % server_address)
+        print('starting up on %s port %s' % server_address)
         self.socket.bind(server_address)
 
         # Listen for incoming connections
-        self.socket.listen(4)
+        self.socket.listen(5)
         self.client_address, self.secret_key = self.setup_connection(secret_key)
 
     def decrypt_message(self, cipher_text):
-        # The data format which will be used here will be "# dancer_id | RTT | offset | raw_data"
+        # The data format currently is: # dancer_id | RTT | offset | raw_data
         decoded_message = base64.b64decode(cipher_text)
         iv = decoded_message[:16]
-        secret_key = bytes(str(self.secret_key), encoding="utf8")
+        secret_key = bytes(str(self.secret_key), encoding='utf8')
 
         cipher = AES.new(secret_key, AES.MODE_CBC, iv)
+
         decrypted_message = cipher.decrypt(decoded_message[16:]).strip()
-        decrypted_message = decrypted_message.decode("utf8")
+        decrypted_message = decrypted_message.decode('utf8')
 
-        decrypted_message = decrypted_message[decrypted_message.find("#") :]
-        decrypted_message = bytes(decrypted_message[1:], "utf8").decode("utf8")
+        decrypted_message = decrypted_message[decrypted_message.find('#'):]
+        decrypted_message = bytes(decrypted_message[1:], 'utf8').decode('utf8')
 
-        messages = decrypted_message.split("|")
+        messages = decrypted_message.split('|')
+
+        # Store the decrypted inputs from each dancer into variables
         dancer_id, RTT, offset, raw_data = messages[:MESSAGE_SIZE]
         return {
-            "dancer_id": dancer_id,
-            "RTT": RTT,
-            "offset": offset,
-            "raw_data": raw_data,
+                'dancer_id': dancer_id, 'RTT': RTT, 'offset': offset, 'raw_data': raw_data
         }
-
-    def run(self):
-        while not self.shutdown.is_set():
-            data = self.connection.recv(1024)
-            if data:
-                try:
-                    self.t2 = time.time()
-                    message = data.decode(FORMAT)
-                    decrypted_message = self.decrypt_message(message)
-                    raw_data = decrypted_message["raw_data"]
-                    if verbose:
-                        print("t2:" + str(self.t2))
-                        print("Raw Data: ", raw_data)
-                    self.send_timestamp()
-
-                except Exception as e:
-                    print(e)
-            else:
-                print("no more data from", self.client_address)
-                self.stop()
-
-    def send_timestamp(self):
-        self.t3 = time.time()
-        timestamp = str(self.t2) + "|" + str(self.t3)
-
-        if verbose:
-            print("t3:" + str(self.t3))
-
-        self.connection.sendall(timestamp.encode())
 
     def setup_connection(self, secret_key):
 
-        # print("No actions for 60 seconds to give time to connect")
-        # self.timer = threading.Timer(self.timeout, self.send_timestamp)
-        # self.timer.start()
-
         # Wait for a connection
-        print("waiting for a connection")
+        print('waiting for a connection')
         self.connection, client_address = self.socket.accept()
 
-        print("Enter the secret key: ")
         if not secret_key:
+            print("Enter the secret key: ")
             secret_key = sys.stdin.readline().strip()
 
-        print("connection from", client_address)
+        print('connection from', client_address)
         if len(secret_key) == 16 or len(secret_key) == 24 or len(secret_key) == 32:
             pass
         else:
@@ -124,6 +84,38 @@ class Server(threading.Thread):
 
         return client_address, secret_key
 
+    def run(self):
+        while not self.shutdown.is_set():
+            data = self.connection.recv(1024)
+            if data:
+                self.t2 = time.time()
+                try:
+                    message = data.decode(FORMAT)
+                    decrypted_message = self.decrypt_message(message)
+                    if verbose:
+                        print('Data received. Timestamp t2: ' + str(self.t2))
+                    print('Received message: ', decrypted_message)
+
+                    dancer_id = decrypted_message["dancer_id"]
+                    raw_data = decrypted_message["raw_data"]
+
+                    self.send_timestamp()
+
+                except Exception as e:
+                    print(e)
+            else:
+                print('no more data from', self.client_address)
+                self.stop()
+
+    def send_timestamp(self):
+        self.t3 = time.time()
+        timestamp = str(self.t2) + '|' + str(self.t3)
+
+        if verbose:
+            print('Sending Data. Timestamp t3: ' + str(self.t3))
+
+        self.connection.sendall(timestamp.encode(FORMAT)
+                )
     def stop(self):
         self.connection.close()
         self.shutdown.set()
@@ -131,49 +123,28 @@ class Server(threading.Thread):
 
 def main(dancer_id, secret_key):
     if dancer_id == 1:
-        dancer_server1 = Server(IP_ADDRESS, PORT_NUM[0], GROUP_ID, secret_key)
+        dancer_server1 = Server(IP_ADDR, PORT_NUM[0], GROUP_ID, secret_key)
         dancer_server1.start()
-        print(
-            "dancer_server1 started: IP address:"
-            + IP_ADDRESS
-            + " Port Number: "
-            + str(PORT_NUM[0])
-            + " Group ID number: "
-            + str(GROUP_ID)
-        )
+        print('Server Started for Dancer 1 on IP_ADDRESS: ' + IP_ADDR + ', PORT NUMBER: ' + str(PORT_NUM[0]))
     if dancer_id == 2:
-        dancer_server2 = Server(IP_ADDRESS, PORT_NUM[1], GROUP_ID, secret_key)
+        dancer_server2 = Server(IP_ADDR, PORT_NUM[1], GROUP_ID, secret_key)
         dancer_server2.start()
-        print(
-            "dancer_server2 started: IP address:"
-            + IP_ADDRESS
-            + " Port Number: "
-            + str(PORT_NUM[1])
-            + " Group ID number: "
-            + str(GROUP_ID)
-        )
-
+        print('Server Started for Dancer 2 on IP_ADDRESS: ' + IP_ADDR + ', PORT NUMBER: ' + str(PORT_NUM[1]))
     if dancer_id == 3:
-        dancer_server3 = Server(IP_ADDRESS, PORT_NUM[2], GROUP_ID, secret_key)
+        dancer_server3 = Server(IP_ADDR, PORT_NUM[2], GROUP_ID, secret_key)
         dancer_server3.start()
-        print(
-            "dancer_server3 started: IP address:"
-            + IP_ADDRESS
-            + " Port Number: "
-            + str(PORT_NUM[2])
-            + " Group ID number: "
-            + str(GROUP_ID)
-        )
+        print('Server Started for Dancer 3 on IP_ADDRESS: ' + IP_ADDR + ', PORT NUMBER: ' + str(PORT_NUM[2]))
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="External Comms")
-    parser.add_argument('-d', '--dancer_id', type=int, help='dancer id', required=True)
-    parser.add_argument('-v', '--verbose', default=False, help='verbose', type=bool)
-    parser.add_argument('-s', '--secret_key', default='cg40024002group2', help='secret key')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Ultra96 Server set-up for Laptops to connect")
+    parser.add_argument('-d', '--dancer_id', type=int, metavar='', required=True, help='dancer id')
+    parser.add_argument('-s', '--secret_key', metavar='', default='cg40024002group2', help='secret key')
+    parser.add_argument('-v', '--verbose', type=bool, default=False, help='verbose')
 
     args = parser.parse_args()
+
     dancer_id = args.dancer_id
-    verbose = args.verbose
     secret_key = args.secret_key
+    verbose = args.verbose
 
     main(dancer_id, secret_key)
